4f92dc83b75c94141e58884795e68d8550ce2cda
kevinkkimutai <kiruikevinkim@gmail.com>
2023-10-19 19:17:31 +0300

    💄  car api generated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
 
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "http://localhost:5173"
+      origins "https://car-nation.vercel.app"
   
       resource "*",
         headers: :any,
