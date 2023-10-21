09e27d95ff6b4df1a099c512d5b1fbbcc6ed2d22
kevinkkimutai <kiruikevinkim@gmail.com>
2023-10-21 15:07:58 +0300

    💄  car api generated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,17 +53,6 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins "https://car-nation.vercel.app"
-  
-      resource "*",
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
-  end
-  
-
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
